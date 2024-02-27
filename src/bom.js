7f77315260d626c845d1ef45a5fc811f6b095541
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-02-27 18:54:12 +1100

    Add attribution of FileReader demo code

--- src/bom.js
+++ src/bom.js
@@ -1,10 +1,10 @@
 // const testBOM = () => console.log("function exists!");
 
+// FileReader demo code source:
+// https://www.w3tweaks.com/how-to-read-a-csv-file-in-javascript.html
+
 function testBOM(sourceFile) {
 	console.log("function exists!");
-	
-
-
 }
 
 function readFile(event) {
