ccee5925d97304c3b5ebadcc9e1c6182e880c542
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-02-27 18:10:58 +1100

    Add file selector element

---
+++ src/bom.js
@@ -0,0 +1,11 @@
+// const testBOM = () => console.log("function exists!");
+
+function testBOM(sourceFile) {
+	console.log("function exists!");
+	
+	let reader = new FileReader();
+
+	
+}
+
+export {testBOM};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js';
+import { testBOM } from './bom.js';
 
 function component() {
 	const element = document.createElement('div');
@@ -15,4 +16,21 @@ function component() {
 	return element;
 }
 
+function fileSelector() {
+	const element = document.createElement('div');
+	const sel = document.createElement('input');
+	sel.setAttribute('type', 'file');
+	sel.setAttribute('id', 'csvFileInput');
+	sel.setAttribute('accept', '.csv');
+
+	element.appendChild(sel);
+
+	return element;
+}
+
 document.body.appendChild(component());
+document.body.appendChild(fileSelector());
+
+// create file selector
+
+testBOM('test_data.csv');
