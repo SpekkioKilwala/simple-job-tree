714bd6e84e4c9a3f42b99c19186e785b3b206c8b
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-02-28 18:13:55 +1100

    remove hello-world webpack testing code

--- src/index.js
+++ src/index.js
@@ -2,20 +2,6 @@ import _ from 'lodash';
 import printMe from './print.js';
 import { testBOM, readFile } from './bom.js';
 
-function component() {
-	const element = document.createElement('div');
-	const btn = document.createElement('button');
-
-	// Lodash, now imported by this script
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	btn.innerHTML = 'Click me and check the console!';
-	btn.onclick = printMe;
-
-	element.appendChild(btn);
-
-	return element;
-}
-
 function fileSelector() {
 	const element = document.createElement('div');
 	const sel = document.createElement('input');
@@ -28,7 +14,6 @@ function fileSelector() {
 	return element;
 }
 
-document.body.appendChild(component());
 document.body.appendChild(fileSelector());
 document.getElementById('csvFileInput').addEventListener('change', readFile);
 
